2f229492eadcd387e76a90c6735154bbb878c72f
Iqbal <iqbalfachrian@gmail.com>
2023-03-11 13:14:31 +0700

    tugas section 4

---
+++ section4.js
@@ -0,0 +1,17 @@
+//section 4 tugas 5
+
+function myFunction() {
+    alert("hello world");
+}
+
+//jawaban
+myFunction();
+
+//section 4 tugas 6
+const person = {
+    firstName: 'john',
+    lastName: 'Doe'
+}
+
+//jawaban
+alert(person.firstName);
