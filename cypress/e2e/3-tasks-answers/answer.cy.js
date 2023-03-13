9d782f94447338624b9a05fbab231f203cbc4847
Iqbal <iqbalfachrian@gmail.com>
2023-03-13 13:02:21 +0700

    add file answer.cy on 3-tasks-answers

--- cypress/e2e/3-tasks-answers/answer.cy.js
+++ cypress/e2e/3-tasks-answers/answer.cy.js
@@ -3,10 +3,10 @@ describe('My first Test', () => {
     it('clicking "type" shows the right headings', () => {
         cy.visit('https://example.cypress.io');
 
-        //cy.pause()
+        cy.pause()
 
         cy.contains('type').click();
-       // cy.wait(2000);
+        //cy.wait(2000);
         cy.url().should('include', '/commands/actions');
         
         cy.get('.action-email')
