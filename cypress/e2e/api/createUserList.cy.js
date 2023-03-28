e59d801facc08568bc33cd92b96a0c824af83ea4
Iqbal <iqbalfachrian@gmail.com>
2023-03-28 14:43:14 +0700

    add tugas create user method POST test API

--- cypress/e2e/api/createUserList.cy.js
+++ cypress/e2e/api/createUserList.cy.js
@@ -1,14 +1,19 @@
+
 describe('Create New User', () => {
     it('Successfully create a new user', () => {
         const user = {
-            "name": "Iqbal",
-            "job": "Junior QA Automation"
+            "firstName": "Iqbal",
+            "lastName": "Fachrian",
+            "job": "Junior QA Automation",
+            "age": 25
         }
 
         cy.request('POST', 'https://reqres.in/api/users', user).then((response) => {
             expect(response.status).to.eq(201);
-            expect(response.body.name).to.eq(user.name);
+            expect(response.body.firstName).to.eq(user.firstName);
+            expect(response.body.lastName).to.eq(user.lastName);
             expect(response.body.job).to.eq(user.job);
+            expect(response.body.age).to.eq(user.age);
         })
     });
 })
