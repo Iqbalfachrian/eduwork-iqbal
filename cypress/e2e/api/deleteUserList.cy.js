cbe313d0d95e66aa25894f743b44e820437964fe
Iqbal <iqbalfachrian@gmail.com>
2023-03-29 13:10:35 +0700

    modified delete user list

--- cypress/e2e/api/deleteUserList.cy.js
+++ cypress/e2e/api/deleteUserList.cy.js
@@ -1,5 +1,6 @@
 it.only('Delete user', () => {
-    cy.request('DELETE', 'https://reqres.in/api/users/540').then((response) => {
+    cy.request('DELETE', 'https://reqres.in/api/users/540').should((response) => {
         expect(response.status).equal(204);
-    })
-})
+        expect(response.body).to.be.empty
+    });
+});
